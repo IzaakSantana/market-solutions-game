71338ce99c61e583314906c8171454946d93acbe
Isaac Santana Souza <isaacsantana.162@gmail.com>
2023-08-23 10:41:57 -0300

    Added functions to chage cursor shape
    Added functions to change cursor shape when interacting with the boxes

--- scripts/objects/box_holder.gd
+++ scripts/objects/box_holder.gd
@@ -9,12 +9,26 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed('left_click') and mouse_in:
+		dragging_cursor()
 		position = get_global_mouse_position()
+	if Input.is_action_just_released("left_click"):
+		pointing_cursor()
 
 
 func _on_mouse_entered():
 	mouse_in = true
+	pointing_cursor()
 
 
 func _on_mouse_exited():
 	mouse_in = false
+	defult_cursor()
+
+func defult_cursor():
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
+
+func pointing_cursor():
+	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+	
+func dragging_cursor():
+	Input.set_default_cursor_shape(Input.CURSOR_DRAG)
