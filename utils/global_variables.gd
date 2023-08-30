3804e416dcf09ba5996fa53fdfee125cae32ceaf
Isaac Santana Souza <isaacsantana.162@gmail.com>
2023-08-30 10:32:37 -0300

    Added some pallete colors
    Added some pallete colors to the global variables

--- utils/global_variables.gd
+++ utils/global_variables.gd
@@ -1,4 +1,14 @@
 extends Node
 
+# General
+var colors = {
+	white = Color8(255, 255, 255),
+	red = Color8(232, 59, 59),
+	yellow = Color8(249, 194, 43),
+	blue = Color8(77, 155, 230),
+	green = Color8(30, 188, 115)
+}
+
 # Level 1
 var box_held = false
+
