d261ab1fab2474bfffefbdc034e73d5301878f5e
Isaac Santana Souza <isaacsantana.162@gmail.com>
2023-09-03 12:18:46 -0300

    Organized script folder

--- scripts/level_01/objects/box_holder.gd
+++ scripts/level_01/objects/box_holder.gd
@@ -6,14 +6,12 @@ var max_weight = 4
 @export var weight = number_generator.randi_range(min_weight, max_weight)
 var mouse_in = false
 var being_held = false
-
+var box_respawn_pos: Vector2
 
 func _ready():
 	$Marker.modulate = GlobalVariables.random_box_color()
 	$Weight.text = str(weight) + " Kg"
-	
-	
-	
+	box_respawn_pos = get_node("../BoxRespawn").position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -27,23 +25,15 @@ func _process(delta):
 
 		if being_held:
 			dragging_cursor()
+			
 			position = get_global_mouse_position()
 
 	if Input.is_action_just_released("left_click"):
+		
 		freeze = false
 		GlobalVariables.box_held = false
 		being_held = false
 		pointing_cursor()
-
-
-func _on_mouse_entered():
-	mouse_in = true
-	pointing_cursor()
-
-
-func _on_mouse_exited():
-	mouse_in = false
-	defult_cursor()
 
 
 func defult_cursor():
@@ -60,3 +50,20 @@ func dragging_cursor():
 
 func can_hold_box():
 	return !GlobalVariables.box_held
+
+
+func _on_mouse_entered():
+	mouse_in = true
+	pointing_cursor()
+
+
+func _on_mouse_exited():
+	mouse_in = false
+	defult_cursor()
+	
+
+#func _on_screen_exited():
+#	freeze = true
+#	position = box_respawn_pos
+#	freeze = false
+	
